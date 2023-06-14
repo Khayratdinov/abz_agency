003deac953a903f5960e2f6e0a958d76d7ba67a3
Khayratdinov <khayratdinov.np@gmail.com>
2023-06-14 10:34:08 +0500

    🗃 1. Created database using Django migrations

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    hire_date = models.DateField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    manager = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    employee_level = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.full_name
