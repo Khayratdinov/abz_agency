06181856af0f9bc642eb17774591aeac687903cc
Khayratdinov <khayratdinov.np@gmail.com>
2023-06-20 22:08:34 +0500

    🔐 authentication system.

--- employees/urls.py
+++ employees/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("employee_list/", views.employee_list, name="employee_list"),
     path("employee_list_ajax/", views.employee_list_ajax, name="employee_list_ajax"),
     path("task_list/", views.task_list, name="task_list"),
+    path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- employees/views.py
+++ employees/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Employee
 from django.http import JsonResponse
 from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -12,6 +16,7 @@ def task_list(request):
     return render(request, "task_list.html")
 
 
+@login_required
 def employee_list(request):
     return render(request, "employee_list.html")
 
@@ -39,3 +44,37 @@ def employee_list_ajax(request):
         )
 
     return JsonResponse({"employees": data})
+
+
+# ============================================================================ #
+
+
+def signup_view(request):
+    print("HELLO")
+    print(request.method)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("employee_list")
+    else:
+        form = UserCreationForm()
+    return render(request, "signup.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("employee_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
