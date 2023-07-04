bfaa40990e36cc9f2adbf6ada5d2ded41f986028
Khayratdinov <khayratdinov.np@gmail.com>
2023-07-04 20:34:22 +0500

    ⚙️ added image and low image

--- employees/models.py
+++ employees/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
+from PIL import Image
+import os
 
 # Create your models here.
 
@@ -16,12 +19,29 @@ class Employee(models.Model):
     employee_level = models.PositiveIntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
+    image = models.ImageField(upload_to="employee_images/")
+    low_image = models.ImageField(upload_to="employee_images/", blank=True)
 
     def __str__(self):
         return self.full_name
 
+    @property
     def get_position_display(self):
         return f"{self.position} ({self.full_name})"
 
     def get_edit_url(self):
         return reverse("update_employee", args=[self.id])
+
+
+def create_low_image(sender, instance, **kwargs):
+    if instance.image:
+        image = Image.open(instance.image.path)
+        low_image_size = (100, 100)
+        image.thumbnail(low_image_size, Image.LANCZOS)
+        low_image_path = instance.image.path.replace(".jpg", "_low.jpg")
+        image.save(low_image_path)
+        instance.low_image.name = low_image_path.replace("employee_images/", "")
+        Employee.objects.filter(id=instance.id).update(low_image=instance.low_image)
+
+
+models.signals.post_save.connect(create_low_image, sender=Employee)
